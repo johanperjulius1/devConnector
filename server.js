15df003bd575790d6e84a5d39bca374c5fe5f5fd
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2021-07-03 00:50:08 +0200

    stting up connection to mongodb database.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const server = express();
 const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 server.get('/', (req, res) => {
   res.send('Hello World!');
